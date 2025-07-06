0e04ffabd952d830e3b3881515c7903b8fdc6bc1
Ayla SC <108141470+Ayla-Coder@users.noreply.github.com>
2025-07-05 20:46:58 -0700

    trying a new link for the google sheets

--- RSVP_script.js
+++ RSVP_script.js
@@ -1183,7 +1183,7 @@ document.addEventListener("DOMContentLoaded", function() {
             formData = gatherFormData(sharedQuestion);
         }
 
-        const url = 'https://script.google.com/macros/s/AKfycbxK2_tXyshCaDX2gynpafcECaMXWY5Pwm3aJ4xbB_0WRvMEApW1h0j29QkCui69CjiTkA/exec';
+        const url = 'https://script.google.com/macros/s/AKfycbx7aL0QcVHviyOmp1zncFni3ZfE1MUN5MyxXcTaEwIy1JuDbj-zsZS_oPztmvR-gUrl/exec';
 
         fetch(url, { method: 'POST', body: JSON.stringify(formData) })
             .then(res => res.json())
